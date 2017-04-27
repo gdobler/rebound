eb216b579fc88e9d4b221a8367005c13b6179102
Greg Dobler <greg.dobler@gmail.com>
2017-04-26 22:23:47 -0400

    removed visualization

--- rebound/utils.py
+++ rebound/utils.py
@@ -3,7 +3,6 @@
 
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 
 def read_header(hdrfile, verbose=True):
     """
@@ -129,41 +128,3 @@ def read_hyper(fpath, fname=None, full=True):
             self.ncol     = sh[2]
 
     return output(fpath)
-
-
-def hyper_viz(cube, img, asp=0.45):
-    """
-    Visualize a hyperspectral data cube.
-    """
-
-    def update_spec(event):
-        if event.inaxes == axim:
-            rind = int(event.ydata)
-            cind = int(event.xdata)
-
-            tspec = cube.data[:, rind, cind]
-            linsp.set_data(cube.waves, cube.data[:, rind, cind])
-            axsp.set_ylim(tspec.min(), tspec.max() * 1.1)
-            axsp.set_title("({0},{1})".format(rind, cind))
-
-            fig.canvas.draw()
-
-
-    # -- set up the plot
-    fig, ax    = plt.subplots(2, 1, figsize=(10, 10))
-    axsp, axim = ax
-
-    # -- show the image
-    axim.axis("off")
-    im = axim.imshow(img, "gist_gray", interpolation="nearest", aspect=asp)
-
-    # -- show the spectrum
-    axsp.set_xlim(cube.waves[0], cube.waves[-1])
-    linsp, = axsp.plot(cube.waves, cube.data[:, 0, 0])
-
-    fig.canvas.draw()
-    fig.canvas.mpl_connect("motion_notify_event", update_spec)
-
-    plt.show()
-
-    return
