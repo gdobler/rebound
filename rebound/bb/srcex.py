aa08bc32bf4ae41266a5905755cda20d68a36c83
Greg Dobler <greg.dobler@gmail.com>
2017-05-19 16:55:42 -0400

    skeleton for the beginnings of source extraction

---
+++ rebound/bb/srcex.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -- get image file list
+
+# -- read images and convert to luminosity (mean along color axis)
+
+# -- stack into 3D array (nrow, ncol, nimg)
+
+# -- subtract mean along nimg axis and divide by stddev along nimg axis
+
+# -- correlate (multiply and mean) with neighboring pixels
