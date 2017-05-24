ce6836a7ad3610b06bb282e9b2472f237f91457a
Crull Daynan <dc3445@compute.cusp.nyu.edu>
2017-05-24 13:18:46 -0400

    added lum convert method to srcex.py

--- rebound/bb/srcex.py
+++ rebound/bb/srcex.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# -- get image file list
+import numpy as np
+import os
 
-# -- read images and convert to luminosity (mean along color axis)
+
+def convert_lum(data_dir):
+    '''
+    Takes as input full filepath for directory of Oct 31, 2013 raw data images
+    
+    Reads raw image file, reshapes into hard coded dimensions 
+    for images from October 2013: 2160 by 4096 pixels in RGB format
+
+    Finds luminosity (mean of values on RGB axis)
+
+    Returns numpy datacube of luminosity values with dimensions: w x h x # of images (about 60)
+
+    '''
+    return  np.array([np.fromfile(os.path.join(data_dir,i),dtype=np.uint8).reshape(2160,4096,3).mean(axis=-1) for i in sorted(os.listdir(data_dir))[::60]])
+
+
+#notes: imgs -= img.mean(i)
 
 # -- stack into 3D array (nrow, ncol, nimg)
 
