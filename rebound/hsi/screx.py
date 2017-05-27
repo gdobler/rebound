63bde03a96e8857e404cd5c870e5cfcf5bc48c21
Akshay Penmatcha <akp418@compute.cusp.nyu.edu>
2017-05-27 14:42:55 -0400

    creating hsi source extraction script

---
+++ rebound/hsi/screx.py
@@ -0,0 +1,12 @@
+
+
+import numpy as np
+import os
+
+def pix_corr(img, trav):
+    '''
+    pix_corr takes an input of the hyperspectral image and the direction in which we want
+    to find the correlation
+
+    
+    '''
