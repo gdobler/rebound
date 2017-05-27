a27f984d47c01489338a5c6136a72ee6f5288c61
Akshay Penmatcha <akp418@compute.cusp.nyu.edu>
2017-05-27 17:14:24 -0400

    adding hsi screx.py algorithm

--- rebound/hsi/screx.py
+++ rebound/hsi/screx.py
@@ -10,8 +10,16 @@ def pix_corr(img, trav):
     
     Args:
         img    :   cube.data
-        trav   :   string 'right', 'left', 'up', 'down'
+        trav   :   {'right', 'left', 'up', 'down'}
     
     '''
     
-    
+    # Code block for converting 3D hyperspectral image to 2D array of correlations
+    # Algorithm-1:
+        # using for loop to traverse across the pixels and calculating the correlation
+        # coefficient using scipy.stats
+        # Trying this in test scripts
+    # Algorithm-2:
+        # Explore np.mean(keepdims=) approach to convert image to 2D array of correlations 
+        # Then Use np.ones(img.shape,dtype=bool) to mask the highly correlated pixels
+        # Repeat this in all 4 directions
