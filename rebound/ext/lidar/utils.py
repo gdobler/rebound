8adc97d852597fd0e9f28cc936748152760aa23c
Greg Dobler <greg.dobler@gmail.com>
2017-05-30 13:56:25 -0400

    first draft of utility to read a raster

--- rebound/ext/lidar/utils.py
+++ rebound/ext/lidar/utils.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import numpy as np
 
 def get_tile_list():
     """
@@ -12,3 +13,29 @@ def get_tile_list():
 
     return fnames
 
+
+def read_raster(rname):
+    """
+    Read in a raster grid.
+    **NOTE:** poor handling of header info at the moment...
+
+    Parameters
+    ----------
+    fname : str
+        The name of the raster file.
+
+    Returns
+    -------
+    rast : ndarray
+        The raster array.
+    """
+
+    # -- read header info
+    for rec in open(rname.replace(".bin", ".hdr"), "r"):
+        if "nrow" in rec:
+            nrow = int(rec.split(":")[1])
+        elif "ncol" in rec:
+            ncol = int(rec.split(":")[1])
+
+    # -- read in raster and reshape
+    return np.fromfile(rname).reshape(nrow, ncol)
