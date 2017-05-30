651614b9b1b804e592334db1325bda18ace8b452
Greg Dobler <greg.dobler@gmail.com>
2017-05-30 13:45:26 -0400

    write raster to file

--- rebound/ext/lidar/rasterize.py
+++ rebound/ext/lidar/rasterize.py
@@ -3,8 +3,35 @@
 
 import os
 import sys
+import datetime
 import numpy as np
 from utils import get_tile_list
+
+def write_header(fname, params):
+    """
+    Write a header for a binary raster file.
+
+    Parameters
+    ----------
+    fname : str
+        The filename of the raster (should end in ".bin").
+    params : dict
+        The parameters to write to the header file.
+    """
+
+    # -- open the file and write output time
+    fopen = open(fname.replace(".bin", ".hdr"), "w")
+    fopen.write("{0}\n".format(str(datetime.datetime.now())))
+
+    # -- loop through parameters
+    for k, v in params.items():
+        fopen.write("{0} : {1}\n".format(k, str(v)))
+
+    # -- close the file
+    fopen.close()
+
+    return
+
 
 # -- set the minmax 
 mm = [[978979.241501, 194479.07369], [1003555.2415, 220149.07369]]
@@ -40,3 +67,9 @@ for ii, fname in enumerate(fnames):
 
 # -- convert sum to mean
 rast /= cnts + (cnts == 0)
+
+# -- write to output
+params = {"nrow" : nrow, "ncol" : ncol}
+oname  = os.path.join(os.environ["REBOUND_WRITE"], "rasters", "MN_raster.bin")
+rast.tofile(oname)
+write_header(oname, params)
