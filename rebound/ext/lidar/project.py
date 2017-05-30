009e708927103c409e6200c0c3cb4699ecd5bedb
Greg Dobler <greg.dobler@gmail.com>
2017-05-30 18:51:39 -0400

    find the closest distance at least as tall as the projected point

--- rebound/ext/lidar/project.py
+++ rebound/ext/lidar/project.py
@@ -14,6 +14,13 @@ rast  = read_raster(fname)
 params = np.array([1.46161933, -1.26155483e-02, 2.39850465e-02, 
                    9.87891059e+05, 1.91728245e+05, 4.00726823e+02, 
                    1.63556904e+04])
+x0     = params[3]
+y0     = params[4]
+
+
+# -- initialize an image
+img = np.zeros((1000, 1000), dtype=float)
+
 
 # -- for each pixel
 #    - identify the x,y coordinates for all zs
@@ -21,22 +28,23 @@ params = np.array([1.46161933, -1.26155483e-02, 2.39850465e-02,
 #    - find all x,y coordinates for which z is greater than projectws line
 #    - of those, find the closest
 
-xx, yy = colin_inv(params, 0.0, 0.0, np.arange(0, 1000., 0.25))
+zs = np.arange(0, 1000., 0.25)
+xx, yy = colin_inv(params, 0.0, 0.0, zs)
 
 mm = [[978979.241501, 194479.07369], [1003555.2415, 220149.07369]]
 
 rind = (yy - mm[0][1]).round().astype(int)
 cind = (xx - mm[0][0]).round().astype(int)
 
-zz = rast[rind, cind]
-
-
-x0 = params[3]
-y0 = params[4]
-
-
-plot(cind*0.1, rind*0.1, '.', color="lime", zorder=3)
-imshow(rast[::10, ::10], "viridis", clim=[0,500])
+tind  = (rind > 0) & (cind > 0)
+rind  = rind[tind]
+cind  = cind[tind]
+xx    = xx[tind]
+yy    = yy[tind]
+tall  = rast[rind, cind] > zs[tind]
+dd    = (xx[tall] - x0)**2 + (yy[tall] - y0)**2
+close = np.arange(rind.size)[tall][dd.argmin()]
+index = [rind[close], cind[close]]
 
 
 
