5b32146874ae2c2272119d81c04b98bd799797f3
Greg Dobler <greg.dobler@gmail.com>
2017-05-31 00:27:00 -0400

    increase the size of the image (and subsample) and increase the maximum height of a building

--- rebound/ext/lidar/project.py
+++ rebound/ext/lidar/project.py
@@ -7,8 +7,13 @@ from utils import read_raster
 from colin_inv import *
 
 # -- load the raster
-fname = os.path.join(os.environ["REBOUND_WRITE"], "rasters", "MN_raster.bin")
-rast  = read_raster(fname)
+try:
+    rast
+except:
+    fname = os.path.join(os.environ["REBOUND_WRITE"], "rasters", 
+                         "MN_raster.bin")
+    print("readin raster {0}...".format(fname))
+    rast  = read_raster(fname)
 
 
 # -- set the camera parameters
@@ -20,8 +25,8 @@ y0     = params[4]
 
 
 # -- initialize an image
-nrow = 500
-ncol = 500
+nrow = 4096
+ncol = 2160
 img = np.zeros((nrow, ncol), dtype=float)
 
 
@@ -31,18 +36,19 @@ img = np.zeros((nrow, ncol), dtype=float)
 #    - find all x,y coordinates for which z is greater than projectws line
 #    - of those, find the closest
 
-zs = np.arange(0, 1000., 0.25)
+zs = np.arange(0, 1500., 0.25)
 mm = [[978979.241501, 194479.07369], [1003555.2415, 220149.07369]]
 
-for ii in range(nrow):
+for ii in range(nrow)[::4]:
     print("\r{0} : {1}".format(ii+1, nrow)),
     sys.stdout.flush()
-    for jj in range(ncol):
+    for jj in range(ncol)[::4]:
 
         xx, yy = colin_inv(params, ii - nrow // 2, jj - ncol // 2, zs)
 
         rind  = (yy - mm[0][1]).round().astype(int)
         cind  = (xx - mm[0][0]).round().astype(int)
+
         tind  = (rind >= 0) & (cind >= 0) & (rind < rast.shape[0]) & \
             (cind < rast.shape[1])
         rind  = rind[tind]
