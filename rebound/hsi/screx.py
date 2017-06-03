703c54a6bdcc24662420a6c0749566020c55160f
Akshay Penmatcha <akypen@gmail.com>
2017-06-02 22:04:48 -0400

    hsi screx code till source mask

--- rebound/hsi/screx.py
+++ rebound/hsi/screx.py
@@ -2,24 +2,36 @@
 
 import numpy as np
 import os
+import utils
 
-def pix_corr(img, trav):
+def hyper_pix_corr(path, fname, thresh=0.5):
     '''
     pix_corr takes an input of the hyperspectral image and the direction in which we want
     to find the correlation
     
     Args:
-        img    :   cube.data
-        trav   :   {'right', 'left', 'up', 'down'}
+        path    :   Path to the directory where hyperspectral images are located
+        fname   :   File name of raw hyperspectral image
+        thresh  :   Correlation threshold for masking the sources
     
     '''
-    
-    # Code block for converting 3D hyperspectral image to 2D array of correlations
-    # Algorithm-1:
-        # using for loop to traverse across the pixels and calculating the correlation
-        # coefficient using scipy.stats
-        # Trying this in test scripts
-    # Algorithm-2:
-        # Explore np.mean(keepdims=) approach to convert image to 2D array of correlations 
-        # Then Use np.ones(img.shape,dtype=bool) to mask the highly correlated pixels
-        # Repeat this in all 4 directions
+        
+    # Reading the Raw hyperspectral image
+    cube = utils.read_hyper(path, fname)
+
+    # Storing the hyperspectral image as a memmap for future computations
+    img = 1 * cube.data
+
+    # Normalizing the Image
+    img -= img.mean(0, keepdims=True)
+    img /= img.std(0, keepdims=True)
+
+    # Computing the correlations between the left-right pixels
+    corr_x = (img[:, :-1] * img[:, 1:]).mean(0)
+
+    # Computing the correlations between the top-down pixels
+    corr_y = (img[:-1, :] * img[1:, :]).mean(0)
+
+    # Creating a Mask for all the pixels/sources with correlation greater than threshold
+    corr_mask_x = corr_x > thresh
+    corr_mask_y = corr_y > thresh
