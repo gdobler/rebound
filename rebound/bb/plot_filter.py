a9af361c36f38936affcc2fda581b5d31ec9b12e
Crull Daynan <dc3445@compute.cusp.nyu.edu>
2017-06-05 01:12:44 -0400

    update plot_filter

--- rebound/bb/plot_filter.py
+++ rebound/bb/plot_filter.py
@@ -6,10 +6,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_image(img, mask=None, clim=None, oname=None):
+def plot_image(mask, img=None, clim=None, oname=None):
     """
-    Visualize a broadband image.
-    Mask for pixel correlations.
+    Plot mask arrays.
     """
 
     # -- check for filename input (need to add read_raw to a utils file)
@@ -18,22 +17,21 @@ def plot_image(img, mask=None, clim=None, oname=None):
 
     # -- set up the plot
     xs = 5.0
-    ys = xs * float(img.shape[0]) / float(img.shape[1])
-    fig, ax = plt.subplots(2,1,figsize=(xs, ys))
-    fig.subplots_adjust(0, 0, 1, 1)
-    axr,axf = ax
-    axr.axis("off")
-    axf.axis("off")
+    ys = xs * float(mask.shape[0]) / float(mask.shape[1])
+    fig = plt.figure(figsize=(xs, ys))
+    # fig.subplots_adjust(0, 0, 1, 1)
+    # ax1,ax2 = ax
+    # ax1.axis("off")
+    # ax2.axis("off")
 
-    # -- create filter with alpha channel masked to pixel correlations
-    filt_img = np.zeros((img.shape[0]-1,img.shape[1]-1),dtype=np.uint8)
-
+    # -- plot mask
+    filt_img = np.zeros((mask.shape[0],mask.shape[1]),dtype=np.uint8)
     filt_img[mask] = 255
 
 
     # -- show the image
-    im = axr.imshow(img, cmap='gist_gray',clim=clim)
-    mask = axf.imshow(filt_img,cmap='gist_gray',clim=clim)
+    # im = axr.imshow(img, cmap='gist_gray',clim=clim)
+    mask = plt.imshow(filt_img,cmap='gist_gray',clim=clim)
     fig.canvas.draw()
     plt.show()
 
