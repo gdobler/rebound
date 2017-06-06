0c0478e989c1c1a18ce18b72c37b93576d705af2
Anastasia Shegay <as10790@nyu.edu>
2017-06-06 03:12:55 -0400

    stacking raster tiles

---
+++ rebound/ext/lidar/raster_stack.py
@@ -0,0 +1,28 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import gdal
+import glob
+import numpy as np
+from gdalconst import *
+
+flist = glob.glob('*.TIF')
+
+origin = (0, 0)
+result = np.zeros((2048, 2048))
+
+for fname in (sorted(flist)):
+    ds = gdal.Open(fname, GA_ReadOnly)
+    geotransform = ds.GetGeoTransform()
+    tile_origin = geotransform[0], geotransform[3]
+    raster = ds.ReadAsArray()
+    if tile_origin[0] == origin[0] + 2048 and tile_origin[1] == origin[1]: 
+        result = np.vstack((result, raster))
+    elif tile_origin[0] == origin[0] and tile_origin[1] == origin[1] - 2048: 
+        result = np.hstack((result, raster))
+    else:
+        pass
+    origin = tile_origin
+
+print result.shape
