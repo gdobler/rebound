2d386454c887013cccbe82791322ea77011c743b
Anastasiya Shegay <as10790@compute.cusp.nyu.edu>
2017-06-13 10:27:52 -0400

    fixed path

--- rebound/ext/lidar/raster_stack.py
+++ rebound/ext/lidar/raster_stack.py
@@ -8,8 +8,9 @@ import glob
 import numpy as np
 from gdalconst import *
 
-fpath = os.path.join(os.environ['REBOUND_DATA'], "data_3d", "DA18_Raster_Files", "DA18_Raster_Files")
-flist = glob.glob('%s*.TIF') % (fpath)
+fpattern = os.path.join(os.environ['REBOUND_DATA'], "data_3d", "DA18_Raster_Files", "DA18_Raster_Files", "DA18RasterTile")
+fpath = "%s*.TIF" % (fpattern)
+flist = glob.glob(fpath)
 
 origin = (0, 0)
 result = np.zeros((2048, 2048))
@@ -20,11 +21,11 @@ for fname in (sorted(flist)):
     tile_origin = geotransform[0], geotransform[3]
     raster = ds.ReadAsArray()
     if tile_origin[0] == origin[0] + 2048 and tile_origin[1] == origin[1]: 
-        result = np.vstack((result, raster))
-    elif tile_origin[0] == origin[0] and tile_origin[1] == origin[1] - 2048: 
         result = np.hstack((result, raster))
+    elif tile_origin[0] == origin[0] and tile_origin[1] == origin[1] - 2048: 
+        result = np.vstack((result, raster))
     else:
         pass
     origin = tile_origin
 
-print result.shape
+#print result.shape
