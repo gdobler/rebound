c735d9557a83f408f43cde81996fb7d53e2f9238
Crull Daynan <dc3445@compute.cusp.nyu.edu>
2017-06-14 21:28:27 -0400

    cleaned up srcex.py

--- rebound/bb/srcex.py
+++ rebound/bb/srcex.py
@@ -8,7 +8,7 @@ from scipy.ndimage.filters import gaussian_filter as gf
 from scipy.ndimage import measurements as mm
 
 
-def convert_lum(data_dir, skip=6, thresh=.99, usb=False, sh=None, i_start=900, i_stop=-900, gfilter=None):
+def create_mask(data_dir, step, thresh, bk, i_start, i_stop, gfilter):
     '''
     Converts a series of raw images into a 2-D boolean mask array
     that indicates pixels that are highly correlated based on 
@@ -21,21 +21,16 @@ def convert_lum(data_dir, skip=6, thresh=.99, usb=False, sh=None, i_start=900, i
         full filepath of directory with images to be processed
         - requires RBG format
 
-    skip = float (default 6)
-        indicates number of images to skip when loading to aggregate in time
+    step = int (default 6)
+        indicates step size when iterating through files in data_dir
 
-    thresh = float (default .99)
+    thresh = float (default .50)
         threshold correlation coefficient value, below which all pixels are masked
 
-    usb = bool (default False)
-        indicates if images are from usb camera (i.e. 2013 images) or new (i.e. May 2017)
+    bk = bool (default True)
+        indicates if images are from BK (i.e. May 2017) camera or (i.e. 2013 images)
         - if true images are 2160 by 4096 and raw images are in RBG format
         - if false, images are 3072 by 4096 and in monochrome8 format
-
-
-
-    sh = tuple of ints (default None)
-        if not usb, this should be dimensions of raw images (nrow,ncols,rbb=3)
 
     i_start = int (default None)
         index of files in source directory to start loading at 
@@ -52,167 +47,119 @@ def convert_lum(data_dir, skip=6, thresh=.99, usb=False, sh=None, i_start=900, i
     ________
     2-d numpy array
         boolean array that expresses true for coordinates of pixels that are correlated
-        with neighbors
-
-    Note this output can be input into the script "plot_filter.py" to compare this mask image
-    with an original raw image.
-
+        with neighbors.
     '''
-    start = time.time()
-    if usb:
-        sh = (2160, 4096, 3)
-        imgs = np.array([np.fromfile(os.path.join(data_dir, i), dtype=np.uint8).reshape(
-            sh[0], sh[1], sh[2]).mean(axis=-1) for i in sorted(os.listdir(data_dir))[::skip]])
+    start_mask = time.time()
+    print "Extracting image files..."
+
+    if bk:
+        sh = (3072, 4096)
+        imgs_list = []
+        for i in sorted(os.listdir(data_dir))[i_start:i_stop:step]:
+            if i.split('.')[-1] == 'raw':
+                imgs_list.append(np.memmap(os.path.join(
+                    data_dir, i), np.uint8, mode="r").reshape(sh[0], sh[1]))
+        imgs = np.array(imgs_list, dtype='float64')
 
     else:
-        sh = (3072, 4096)
+        sh = (2160, 4096, 3)
+        imgs = np.array([np.fromfile(os.path.join(data_dir, i), dtype=np.uint8).reshape(
+            sh[0], sh[1], sh[2]).mean(axis=-1) for i in sorted(os.listdir(data_dir))[::step]])
 
-        imgs_list = []
-        for i in sorted(os.listdir(data_dir))[i_start:i_stop:skip]:
-            if i.split('.')[-1] == 'raw':
-                # imgs_list.append(np.fromfile(os.path.join(data_dir, i), dtype=np.uint8).reshape(
-                #         sh[0],sh[1]))
+    # slice off last row and col to match mask array
+    img_cube = imgs[:, :-1, :-1].copy()
 
-                imgs_list.append(np.memmap(os.path.join(data_dir, i), np.uint8, mode="r").reshape(sh[0], sh[1]))
+    time_extract = time.time()
 
-        imgs = np.array(imgs_list,dtype='float64')
-
-    time_mean = time.time()
-
-    print "Time to extract data and calculate mean: {}".format(time_mean - start)
-
-    raw_cube = imgs[:,:-1,:-1].copy() # slice off last row and col to match mask array
+    print "Time to extract data and calculate mean: {}".format(time_extract - start_mask)
 
     # run Gaussian filter (options)
     if gfilter is not None:
+        print "Running Gaussian filter..."
         imgs_sm = gf(1.0 * imgs, (gfilter, 0, 0))
         imgs = 1.0 * imgs - imgs_sm
 
-    # -- subtract mean along nimg axis
+    time_standard_st = time.time()
+    print "Standardizing luminosity along time domain..."
+
+    # subtract mean along nimg axis
     imgs -= imgs.mean(axis=0, keepdims=True)
 
-    # - divide by array of standard deviation for each pixel time series
+    # divide by array of standard deviation for each pixel time series
     imgs /= imgs.std(axis=0, keepdims=True)
 
     # this will create nan values for zero division (pixels with
     # unchanging luminosity i.e. 0 st. dev)
     # boolean mask and set nan values to 0 for further operations
     img_idx = np.isnan(imgs)
-
     imgs[img_idx] = 0
 
-    time_mask = time.time()
-    print "Time to stack, subtract mean and divide by std, create mask for nan values: {}".format(time_mask-time_mean)
-    
-    # create empty matrix for horizontal and vertical correlation
-    # cor_matrix = np.empty((sh[0], sh[1], 2))
+    time_standard = time.time()
+    print "Time to standardize: {}".format(time_standard-time_standard_st)
+    print "Calculating correlation coefficients..."
 
-    corr_x = (imgs[:,:-1,:] * imgs[:,1:,:]).mean(0)
-    corr_y = (imgs[:,:,:-1] * imgs[:,:,1:]).mean(0)
+    # matrix mult to get horizontal and vertical correlation
+    corr_x = (imgs[:, :-1, :] * imgs[:, 1:, :]).mean(0)
+    corr_y = (imgs[:, :, :-1] * imgs[:, :, 1:]).mean(0)
 
-    # algorithm to calculate horizontal and vertical correlation
-    # for i in range(0, sh[0]-1):
-    #     for j in range(0, sh[1]-1):
-    #         if j < sh[1]-1:
-    #             # left-right correlation across columns
-    #             cor_matrix[i, j][0] = np.dot(
-    #                 imgs[i, j, :], imgs[i, j+1, :].T)/imgs.shape[2]
-    #         if i < sh[0]-1:
-    #             # up-down correlation across rows
-    #             cor_matrix[i, j][1] = np.dot(
-    #                 imgs[i, j, :], imgs[i+1, j, :].T)/imgs.shape[2]
-    time_cor = time.time()
-    print "Time to calculate correlation-cube: {}".format(time_cor-time_mask)
-
-    # mask out negative correlation, and from this
-    # filter to only correlations (in either dimension) above thresh
-
-    # produces a 3d mask array that expresses True if a pixels rightward or downward correlation is above thresh
-    # nrows by ncols by correlation in both directions
-    # cm = cor_matrix > thresh
-
-    time_final_mask = time.time()
-    # final_mask = np.zeros((sh[0], sh[1]), dtype=bool)
-    # assigns pixel and its neighbor as true (there's a more pythonesque way
-    # to do this)
-    # for i in range(0, sh[0]-1):
-    #     for j in range(0, sh[1]-1):
-    #         if cm[i, j, 0]:
-    #             final_mask[i, j] = True
-    #             final_mask[i, j+1] = True
-    #         if cm[i, j, 1]:
-    #             final_mask[i, j] = True
-    #             final_mask[i+1, j] = True
-
-    # Creating a Mask for all the pixels/sources with correlation greater than threshold
-    corr_mask_x = corr_x[:,:-1] > thresh
-    corr_mask_y = corr_y[:-1,:] > thresh
+    # Creating a mask for all the pixels/sources with correlation greater than
+    # threshold
+    corr_mask_x = corr_x[:, :-1] > thresh
+    corr_mask_y = corr_y[:-1, :] > thresh
 
     # Merging the correlation masks in left-right and top-down directions
-    final_mask = corr_mask_x | corr_mask_y
+    mask_array = corr_mask_x | corr_mask_y
+
+    stop_mask = time.time()
+    print "Time to create final image mask: {}".format(stop_mask-time_standard)
+    print "Total create mask runtime: {}".format(stop_mask-start_mask)
+    return mask_array, img_cube
 
 
-    stop = time.time()
-    print "Time to create final image mask: {}".format(stop-time_final_mask)
+def light_curve(data_dir, step=5, thresh=.50, bk=True, i_start=900, i_stop=-900, gfilter=None):
+    '''
+    Calls create_mask() and uses output to label pixels to unique light sources.
+    Averages the luminosity among pixels of each light source
+    to produce lightcurve for each source.
+    '''
+    start = time.time()
 
-    print "Total runtime: {}".format(stop-start)
-    return final_mask, raw_cube
+    # create mask array
+    mask_array, img_cube = create_mask(
+        data_dir, step, thresh, bk, i_start, i_stop, gfilter)
 
-
-def agg_src(mask_array):
-    """
-    Takes as input the output of .conver_lum() -- mask array of correlated pixels.
-    Groups them into contiguous groups, assigns unique labels to the groups,
-    Counts number and size of groups <-> light sources.
-    """
+    print "Labeling light sources..."
 
     # measurements.label to assign sources
-    labels,num_features = mm.label(mask_array.astype(bool))
+    labels, num_features = mm.label(mask_array.astype(bool))
 
     unique, counts = np.unique(labels, return_counts=True)
 
-    # f = np.vectorize(lambda x: int(counts[x-1]))
-
-    # label_size = f(labels.astype(int))
-
-    #return np.dstack((labels,label_size)) # nrows x ncols x [label,labelsize]
-    return labels,unique,counts
-
-
-def light_curve(data_dir, skip=6, thresh=.99, usb=False, sh=None, i_start=900, i_stop=-900, gfilter=None):
-    '''
-    Takes as input the output of agg_src(): set of labels, labelsize and oringial datacube.
-    Averages the luminosity among pixels of each light source
-    to produce lightcurve for each source.
-
-    '''
-
-    # mask datacube with source_array to average luminosity 
-    # of each source, iterating through each image in the series
-    
-    # idx = np.unique(source_array)
-
-    mask,cube = convert_lum(data_dir, skip, thresh, usb, sh, i_start, i_stop, gfilter)
-
-    labels,unique,counts = agg_src(mask)
-
+    # labels,unique,counts = agg_src(mask)
+    time_label = time.time()
+    print "Time to label: {}".format(time_label - start)
+    print "Aggregate light source pixel intensities and create time series array..."
     source_ts = []
 
-    for i in range(0,cube.shape[0]-1):
-        src_sum = mm.sum(cube[i,:,:]*1.0,labels,index=unique[1:])
+    for i in range(0, cube.shape[0]-1):
+        src_sum = mm.sum(cube[i, :, :]*1.0, labels, index=unique[1:])
         source_ts.append(src_sum*1.0/counts[1:])
         # source_ts.append(src_sum*1.0)
 
-    class output():
-        def __init__(self):
-            self.fpath    = data_dir
-            self.cube     = cube
-            self.mask     = mask
-            self.labels   = labels
-            self.unique   = unique
-            self.counts   = counts
-            self.curves   = np.stack(source_ts)
+    time_output = time.time()
+    print "Time to create time series array: {}".format(time_output - time_label)
 
-    # returns array of average luminosity of light source over time:
-    #nimgs x nlightsources
+    class output():
+
+        def __init__(self):
+            self.fpath = data_dir
+            self.cube = cube
+            self.mask = mask
+            self.labels = labels
+            self.unique = unique
+            self.counts = counts
+            self.curves = np.stack(source_ts)
+
+    end = time.time()
+    print "Total runtime: {}".format(end - start)
     return output()
