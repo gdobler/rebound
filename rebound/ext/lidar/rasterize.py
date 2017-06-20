4398820740d7f520704045df144aeb380ea4a345
Greg Dobler <greg.dobler@gmail.com>
2017-06-20 13:56:06 -0400

    pull out main code

--- rebound/ext/lidar/rasterize.py
+++ rebound/ext/lidar/rasterize.py
@@ -32,45 +32,45 @@ def write_header(fname, params):
 
     return
 
-
-# -- set the minmax 
-mm = [[978979.241501, 194479.07369], [1003555.2415, 220149.07369]]
-
-# -- get the range
-nrow = int(round(mm[1][1] - mm[0][1] + 0.5)) + 1
-ncol = int(round(mm[1][0] - mm[0][0] + 0.5)) + 1
-
-# -- initialize the raster and counts
-rast = np.zeros((nrow, ncol), dtype=float)
-cnts = np.zeros((nrow, ncol), dtype=float)
-
-# -- set the tile names
-fnames = get_tile_list()
-nfiles = len(fnames)
-
-# -- read the tiles
-for ii, fname in enumerate(fnames):
-    print("\rworking on tile {0:3} of {1}".format(ii+1, nfiles)),
-    sys.stdout.flush()
-    tile = np.load(fname)
-
-    # -- snap to grid
-    rind = (tile[:, 1] - mm[0][1]).round().astype(int)
-    cind = (tile[:, 0] - mm[0][0]).round().astype(int)
-
-    # -- get the counts in each bin
-    tcnt = np.bincount(cind + rind * ncol)
-
-    # -- update the raster
-    rast[rind, cind] += tile[:, 2]
-    cnts[rind, cind] += tcnt[tcnt > 0]
-
-# -- convert sum to mean
-rast /= cnts + (cnts == 0)
-
-# -- write to output
-params = {"nrow" : nrow, "ncol" : ncol, "rmin" : mm[0][1], "rmax" : mm[1][1], 
-          "cmin" : mm[0][0], "cmax" : mm[1][0]}
-oname  = os.path.join(os.environ["REBOUND_WRITE"], "rasters", "MN_raster.bin")
-rast.tofile(oname)
-write_header(oname, params)
+if __name__ == "__main__":
+    # -- set the minmax 
+    mm = [[978979.241501, 194479.07369], [1003555.2415, 220149.07369]]
+    
+    # -- get the range
+    nrow = int(round(mm[1][1] - mm[0][1] + 0.5)) + 1
+    ncol = int(round(mm[1][0] - mm[0][0] + 0.5)) + 1
+    
+    # -- initialize the raster and counts
+    rast = np.zeros((nrow, ncol), dtype=float)
+    cnts = np.zeros((nrow, ncol), dtype=float)
+    
+    # -- set the tile names
+    fnames = get_tile_list()
+    nfiles = len(fnames)
+    
+    # -- read the tiles
+    for ii, fname in enumerate(fnames):
+        print("\rworking on tile {0:3} of {1}".format(ii+1, nfiles)),
+        sys.stdout.flush()
+        tile = np.load(fname)
+    
+        # -- snap to grid
+        rind = (tile[:, 1] - mm[0][1]).round().astype(int)
+        cind = (tile[:, 0] - mm[0][0]).round().astype(int)
+    
+        # -- get the counts in each bin
+        tcnt = np.bincount(cind + rind * ncol)
+    
+        # -- update the raster
+        rast[rind, cind] += tile[:, 2]
+        cnts[rind, cind] += tcnt[tcnt > 0]
+    
+    # -- convert sum to mean
+    rast /= cnts + (cnts == 0)
+    
+    # -- write to output
+    params = {"nrow" : nrow, "ncol" : ncol, "rmin" : mm[0][1], "rmax" : mm[1][1], 
+              "cmin" : mm[0][0], "cmax" : mm[1][0]}
+    oname  = os.path.join(os.environ["REBOUND_WRITE"], "rasters", "MN_raster.bin")
+    rast.tofile(oname)
+    write_header(oname, params)
