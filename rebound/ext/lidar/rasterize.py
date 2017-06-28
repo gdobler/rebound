788cc92128c2a8090f347935b30ff3c74af9a721
Greg Dobler <greg.dobler@gmail.com>
2017-06-28 00:54:14 -0400

    rasterize lidar

--- rebound/ext/lidar/rasterize.py
+++ rebound/ext/lidar/rasterize.py
@@ -7,6 +7,7 @@ import datetime
 import glob
 import numpy as np
 import pandas as pd
+import laspy as lp
 from utils import get_tile_list
 from raster_stack import get_origin_minmax
 
@@ -85,3 +86,58 @@ def get_lidar_tiles(location="1MT"):
         (coords.ymin >= mm[1]) & (coords.ymax < mm[3])
 
     return coords[ind]
+
+
+def rasterize_lidar():
+    """
+    Rasterize the LiDAR tiles.
+    """
+
+    # -- get the lidar tile names and range
+    coords = get_lidar_tiles()
+    flist  = coords.filename.values
+    nfiles = len(flist)
+    mm     = [[coords.xmin.min(), coords.ymin.min()], 
+              [coords.xmax.max(), coords.ymax.max()]]
+    
+    # -- get the range
+    nrow = int(round(mm[1][1] - mm[0][1] + 0.5)) + 1
+    ncol = int(round(mm[1][0] - mm[0][0] + 0.5)) + 1
+    
+    # -- initialize the raster and counts
+    rast = np.zeros((nrow, ncol), dtype=float)
+    cnts = np.zeros((nrow, ncol), dtype=float)
+    
+    # -- read the tiles
+    for ii, fname in enumerate(flist):
+        print("\rworking on tile {0:3} of {1}".format(ii + 1, nfiles)),
+        sys.stdout.flush()
+        las  = lp.file.File(fname, mode="r")
+        tile = np.vstack((las.x, las.y, las.z)).T
+ 
+        # -- snap to grid
+        rind = (tile[:, 1] - mm[0][1]).round().astype(int)
+        cind = (tile[:, 0] - mm[0][0]).round().astype(int)
+    
+        # -- get the counts in each bin
+        tcnt = np.bincount(cind + rind * ncol)
+    
+        # -- update the raster
+        rast[rind, cind] += tile[:, 2]
+        cnts[rind, cind] += tcnt[tcnt > 0]
+
+        # -- close las file
+        las.close()
+    
+    # -- convert sum to mean
+    rast /= cnts + (cnts == 0)
+    
+    # -- write to output
+    params = {"nrow" : nrow, "ncol" : ncol, "rmin" : mm[0][1], 
+              "rmax" : mm[1][1], "cmin" : mm[0][0], "cmax" : mm[1][0]}
+    oname  = os.path.join(os.environ["REBOUND_WRITE"], "rasters", 
+                          "BK_raster.bin")
+    rast.tofile(oname)
+    write_header(oname, params)
+
+    return
