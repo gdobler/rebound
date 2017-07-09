ddea7ea10d5dd88802dcfda95e9fca3e39d04b96
Crull Daynan <dc3445@compute.cusp.nyu.edu>
2017-07-09 02:07:08 -0400

    srcex cleanup

--- rebound/bb/srcex.py
+++ rebound/bb/srcex.py
@@ -9,59 +9,82 @@ import matplotlib.pyplot as plt
 from scipy.ndimage.filters import gaussian_filter as gf
 from scipy.ndimage import measurements as mm
 
-def find_night(data_dir,step=100):
+def find_night(data_dir,step=100,plot=False):
     '''
     Extract files and plots avg luminosity. 
     Upon click, prints file number (to indicate where to start loading)
     Upon release, prints file number (to indicate where to stop loading)
+    Parameters:
+    ___________
+    data_dir = str
+        full filepath of directory with images to be processed
+        - requires RBG format
+
+    step = int (default 100)
+        indicates step size when iterating through files in data_dir
+
+    plot = boolean (default False)
+        if True, method will plot average luminosity (of each image) over time
+        to allow for visual selection of optimal file start and stop
+
+        if False, method will determine optimal start and stop analytically
+
+
+    Returns:
+    ________
+
+    file_start,file_stop
+
+    if plot = True, draws plot
     '''
-    def vline_st(event):
-        if event.inaxes == ax:
-            cind = int(event.xdata)
-
-            ax.axvline(cind,color='g')
-               
-            ax.set_title("File #: {0}".format(cind))
-
-            fig.canvas.draw()
-
-            print "Start at file #: {}".format(cind*step)
-
-    def vline_end(event):
-        if event.inaxes == ax:
-            cind = int(event.xdata)
-
-            ax.axvline(cind,color='r')
-               
-            ax.set_title("File #: {0}".format(cind))
-
-            fig.canvas.draw()
-
-            print "End at file #: {}".format(cind*step)
-
-
     lum_list = []
-    raw_name_list = []
 
-    for i in sorted(os.listdir(data_dir)):
-        if i.split('.')[-1] == 'raw':
-            raw_name_list.append(i)
+    for i in sorted(os.listdir(data_dir))[::step]:
+        lum_list.append(np.memmap(os.path.join(data_dir, i), dtype=np.uint8, mode='r'))
+        lum_means = np.array(lum_list,dtype=np.float32).mean(1)
 
-    for i in raw_name_list[::step]:
-            lum_list.append(np.memmap(os.path.join(data_dir, i), np.uint8, mode="r"))
+    if not plot: # crude analytical method
+        thresh = np.median(lum_means)+0.5
+        file_start = np.where(lum_means<thresh)[0][0]*step+step
+        file_stop = np.where(lum_means<thresh)[0][-1]*step
 
-    lums = np.array(lum_list,dtype='float32').mean(1)
+        return file_start,file_stop
 
-    # -- set up the plots
-    fig,ax = plt.subplots(1,figsize=(15, 5))
-    im = ax.plot(lums)
+    else: # plot method
 
-    fig.canvas.draw()
-    # fig.canvas.mpl_connect("motion_notify_event", update_spec)
-    fig.canvas.mpl_connect('button_press_event', vline_st)
-    fig.canvas.mpl_connect('button_release_event', vline_end)
-    plt.show()
+        def vline_st(event):
+            if event.inaxes == ax:
+                cind = int(event.xdata)
 
+                ax.axvline(cind,color='g')
+                   
+                ax.set_title("File #: {0}".format(cind))
+
+                fig.canvas.draw()
+
+                print "File start: {}".format(cind*step) + 100
+
+        def vline_end(event):
+            if event.inaxes == ax:
+                cind = int(event.xdata)
+
+                ax.axvline(cind,color='r')
+                   
+                ax.set_title("File #: {0}".format(cind))
+
+                fig.canvas.draw()
+
+                print "File stop: {}".format(cind*step)
+
+        # -- set up the plots
+        fig,ax = plt.subplots(1,figsize=(15, 5))
+        im = ax.plot(lum_means)
+
+        fig.canvas.draw()
+        # fig.canvas.mpl_connect("motion_notify_event", update_spec)
+        fig.canvas.mpl_connect('button_press_event', vline_st)
+        fig.canvas.mpl_connect('button_release_event', vline_end)
+        plt.show()
 
 
 def create_mask(data_dir, step, thresh, bk, file_start, file_stop, gfilter):
@@ -106,18 +129,21 @@ def create_mask(data_dir, step, thresh, bk, file_start, file_stop, gfilter):
         with neighbors.
     '''
     start_mask = time.time()
+    if file_start is None:
+        print "Finding night images..."
+
+        file_start,file_stop = find_night(data_dir)
+        end_night = time.time()
+        print "Time to find night: {}".format(end_night-start_mask)
+
+    start_extract = time.time()
     print "Extracting image files..."
 
     if bk:
         sh = (3072, 4096)
         imgs_list = []
-        raw_name_list = []
-        for i in sorted(os.listdir(data_dir)):
-            if i.split('.')[-1] == 'raw':
-                raw_name_list.append(i)
 
-
-        for i in raw_name_list[file_start:file_stop:step]:
+        for i in sorted(os.listdir(data_dir))[file_start:file_stop:step]:
             imgs_list.append(np.fromfile(os.path.join(
                     data_dir, i), dtype=np.uint8).reshape(sh[0], sh[1]))
         imgs = np.array(imgs_list,dtype=np.float32)
@@ -133,13 +159,13 @@ def create_mask(data_dir, step, thresh, bk, file_start, file_stop, gfilter):
 
     time_extract = time.time()
 
-    print "Time to extract data and calculate mean: {}".format(time_extract - start_mask)
+    print "Time to extract data and calculate mean: {}".format(time_extract - start_extract)
 
     # run Gaussian filter (options)
     if gfilter is not None:
         print "Running Gaussian filter..."
-        imgs_sm = gf(imgs.astype(np.float32), (gfilter, 0, 0))
-        imgs = imgs.astype(np.float32) - imgs_sm
+        imgs_sm = gf(imgs.astype(np.float64), (gfilter, 0, 0))
+        imgs = imgs.astype(np.float64) - imgs_sm
 
     time_standard_st = time.time()
     print "Standardizing luminosity along time domain..."
@@ -179,7 +205,7 @@ def create_mask(data_dir, step, thresh, bk, file_start, file_stop, gfilter):
     return mask_array, img_cube
 
 
-def light_curve(data_dir, step=5, thresh=.50, bk=True, file_start=300, file_stop=3100, gfilter=None):
+def light_curve(data_dir, step=5, thresh=.50, bk=True, file_start=100, file_stop=2800, gfilter=None,pickle_it=None):
     '''
     Calls create_mask() and uses output to label pixels to unique light sources.
     Averages the luminosity among pixels of each light source
@@ -197,7 +223,7 @@ def light_curve(data_dir, step=5, thresh=.50, bk=True, file_start=300, file_stop
 
     unique, counts = np.unique(labels, return_counts=True)
 
-    print "Aggregate light source pixel intensities and create time series array..."
+    print "Creating time series array..."
     source_ts = []
 
     for i in range(0, img_cube.shape[0]):
@@ -225,18 +251,49 @@ def light_curve(data_dir, step=5, thresh=.50, bk=True, file_start=300, file_stop
     time_output = time.time()
     print "Time to create time series cube: {}".format(time_output - time_ts_cube)
 
-    class output():
+    if pickle_it != None:
+        # cube
+        with open(os.path.join(pickle_it,'img_cube.pickle'),'wb') as handle:
+            pickle.dump(img_cube,handle,protocol=pickle.HIGHEST_PROTOCOL)
 
-        def __init__(self):
-            self.fpath = data_dir
-            self.cube = img_cube
-            self.mask = mask_array
-            self.labels = labels
-            self.unique = unique
-            self.counts = counts
-            self.ts = ts_array
-            self.curves = ts_cube
+        # mask
+        with open(os.path.join(pickle_it,'mask.pickle'),'wb') as handle:
+            pickle.dump(mask_array,handle,protocol=pickle.HIGHEST_PROTOCOL)
 
-    end = time.time()
-    print "Total runtime: {}".format(end - start)
-    return output()
+        # labels
+        with open(os.path.join(pickle_it,'labels.pickle'),'wb') as handle:
+            pickle.dump(labels,handle,protocol=pickle.HIGHEST_PROTOCOL)
+
+        # curves
+        with open(os.path.join(pickle_it,'curves.pickle'),'wb') as handle:
+            pickle.dump(ts_array,handle,protocol=pickle.HIGHEST_PROTOCOL)
+
+        # curves_cube
+        for i in np.arange(0,ts_array.shape[0]-1,10):
+            with open(os.path.join(pickle_it,'curves_cube_{}.pickle'.format(i)),'wb') as handle:
+                pickle.dump(ts_cube[i:i+10,:,:],handle,protocol=pickle.HIGHEST_PROTOCOL)
+
+        end = time.time()
+        print "File loaded in output folder"
+        print "Total runtime: {}".format(end - start)
+
+    else:
+        class output():
+
+            def __init__(self):
+                self.fpath = data_dir
+                self.cube = img_cube
+                self.mask = mask_array
+                self.labels = labels
+                self.unique = unique
+                self.counts = counts
+                self.curves = ts_array
+                self.curves_cube = ts_cube
+
+        end = time.time()
+        print "Total runtime: {}".format(end - start)
+        return output()
+
+
+
+
