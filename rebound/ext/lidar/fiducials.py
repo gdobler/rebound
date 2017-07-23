01181f16ee7d8a84f9eac227f061d439566ad8eb
Anastasiya Shegay <as10790@compute.cusp.nyu.edu>
2017-07-22 22:16:27 -0400

    added new fiducials

--- rebound/ext/lidar/fiducials.py
+++ rebound/ext/lidar/fiducials.py
@@ -2,55 +2,62 @@
 
 import numpy as np
 
-img_fid = np.array([[2144, 656],   # 155 Dean St
-		    [1096, 351],   # 370 4th Ave
-		    [1446, 967],   # 198 Bond St
-		    [1089, 3439],  # 126 13th St
-		    [1542, 4005],  # 241 Hoyt St
-		    [1936, 2910],  # 175 Hoyt St
-		    [1668, 1984]]) # 417 Baltic St
+def get_img_dims(image):
+	'''Get image dimensions: x = column pixels, y = row pixels'''
+	if image == 'bk':
+		dims = np.array([4096, 3072])
+	else:
+		print "Not a valid image"
+		return
+	return dims
 
-ldr_fid = np.array([[9.87856166e+05, 1.89364434e+05, 4.60000000e+01],
-      		    [9.87851166e+05, 1.84582434e+05, 5.80000000e+01],
-        	    [9.87753166e+05, 1.88285434e+05, 8.00000000e+01],
-      		    [9.86039166e+05, 1.83084434e+05, 3.20000000e+01],
-      		    [9.87241166e+05, 1.88546434e+05, 6.50000000e+01],
-      		    [9.87414166e+05, 1.88864434e+05, 4.90000000e+01],
-      		    [9.87556166e+05, 1.88604434e+05, 6.60000000e+01]])
+def get_img_fid(image, center = False):
+	'''Get image pixel location of fiducial points, columns first'''
+	if image == 'bk':
+		img_fid = np.array([[656, 2144],   # 155 Dean St front-left corner
+		   		    [351, 1096],   # 370 4th Ave
+		   		    [967, 1446],   # 198 Bond St
+		   		    [3439, 1089],  # 126 13th St
+		   		    [4005, 1542],  # 241 Hoyt St
+		   		    [2910, 1936],  # 175 Hoyt St
+		   		    [1984, 1668],  # 417 Baltic St
+				    [1894, 2165],  # 155 Dean St back-right corner
+				    [786, 2482],   # 97 Hoyt St
+				    [2934, 2483]]) # 160 Schermerhorn St
 
-img_dim = np.array([3072, 4096])
+	else:
+		print "Not a valid image"
+		return
 
-def center_img_fid(img_dim, img_fid):
+	dims = get_img_dims(image)
 
-	'''Center image fiducial points using image dimensions'''
-	
-	for i in range(np.shape(img_fid)[0]):
-		img_fid[i, 0] = img_fid[i, 0] - (img_dim[0] / 2)
-		img_fid[i, 1] = img_fid[i, 1] - (img_dim[1] / 2)
+	if center == True:
+		img_fid = center_img_fid(dims, img_fid)
+
 	return img_fid
 
-def run(img_fid, ldr_fid, num_iter, params=None):
+def get_ldr_fid(image):
+	'''Get x,y,z of fiducial points in LiDAR'''
+	if image == 'bk':
+		ldr_fid = np.array([[9.87858446e+05, 1.89366284e+05, 4.25000000e+01],
+      				    [9.87851166e+05, 1.84582434e+05, 5.80000000e+01],
+      				    [9.87753166e+05, 1.88285434e+05, 8.00000000e+01],
+      				    [9.86039166e+05, 1.83084434e+05, 3.20000000e+01],
+      				    [9.87241166e+05, 1.88546434e+05, 6.50000000e+01],
+      				    [9.87414166e+05, 1.88864434e+05, 4.90000000e+01],
+      				    [9.87556166e+05, 1.88604434e+05, 6.60000000e+01],
+      				    [9.87671816e+05, 1.89426084e+05, 4.25000000e+01],
+      				    [9.87855866e+05, 1.89793684e+05, 3.98000000e+01],
+      				    [9.87694656e+05, 1.90460144e+05, 7.92000000e+01]])
 
-	''' Run the optimization routine for the given image '''
+def center_img_fid(dims, img_fid):
 
-	if params == None:
-		print "Choosing 1MT UO as initial guess\n"
-		params = np.array([1.56926535e+00, -1.20789690e-01,\
-			-3.05255789e-03, 9.87920425e+05, 1.91912958e+05, \
-			3.85333237e+02, -1.10001068e+04]) 
+	'''Center fiducial points using image dimensions'''
+	center_fid = np.zeros([[img_fid[0], img_fid[1]]) 	
+	
+	for i in range(np.shape(img_fid)[0]):
+		center_fid[i, 0] = img_fid[i, 0] - (dims[0] / 2)
+		center_fid[i, 1] = img_fid[i, 1] - (dims[1] / 2)
+	
+	return center_fid
 
-	xyz_s = ldr_fid
-	xy_t = img_fid
-
-	min_score = 100000000000000
-
-	for i in range(0, num_iter):
-		result = call(params,xyz_s,xy_t)
-		print "params, score", result.x, result.fun
-#		print('doing it {0} {1}'.format(i,result.fun))
-#		import pdb; pdb.set_trace()
-		if (result.fun < min_score):# and (result.x[3] < 980491):
-			min_score = result.fun
-			params = result.x
-
-	return [min_score, params]
