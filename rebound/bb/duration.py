d3a1f0eb1594c52fd9f2dbcd9f022b67a645407f
Crull Daynan <dc3445@compute.cusp.nyu.edu>
2017-08-05 10:45:32 -0400

    quick plotting cleanup

--- rebound/bb/duration.py
+++ rebound/bb/duration.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import time
-import utils2
 import numpy as np
 
 
--- rebound/bb/plotting.py
+++ rebound/bb/plotting.py
@@ -79,21 +79,90 @@ def quick_source_info(labeled_mask, clim=None, oname=None):
     return
 
 
-def time_series(ts_array, clim=None, oname=None):
+def plot_curves(curves_obj, clim=None, oname=None):
     """
-    Plots on/off transitions of light sources
+    Plot light curves
+    """
+
+    def update_ts(event):
+        if event.inaxes == axim:
+            rind = int(event.ydata)
+            cind = int(event.xdata)
+
+            tspec = curves_obj.curves[:, rind, cind]
+            linsp.set_data(np.asarray(np.arange(curves_obj.curves.shape[0])),curves_obj.curves[:, rind, cind])
+            axlc.set_ylim(tspec.min(), tspec.max() * 1.1)
+            axlc.set_title("({0},{1})".format(rind, cind))
+
+            fig.canvas.draw()
+
+    # -- plot mask
+    mask = np.zeros(curves_obj.mask.shape,dtype=np.uint8)
+    mask[curves_obj.mask] = 255
+
+
+    # -- set up the plot
+    fig, ax = plt.subplots(2, 1, figsize=(10, 10))
+    axlc, axim = ax
+
+    # -- show the image
+    axim.axis("off")
+    im = axim.imshow(mask,cmap='gist_gray',clim=clim)
+    axim.set_title('Mask Image')
+
+    # -- show the lightcurve
+    axlc.set_xlim(0, curves_obj.curves.shape[0])
+    linsp, = axlc.plot(np.asarray(np.arange(curves_obj.curves.shape[0])),curves_obj.curves[:, 0, 0])
+
+    fig.canvas.draw()
+    fig.canvas.mpl_connect("motion_notify_event", update_ts)
+
+    plt.show()
+
+    # -- write to file if desired
+    if oname is not None:
+        fig.savefig(oname, format='png',clobber=True)
+
+    return
+
+def four_ts(ts1, ts2, ts3, ts4, clim=None, oname=None):
+    """
+    Plots time series (i.e. lightcurve signal, on/off detection, etc)
+    """
+
+    # -- set up the plot
+    fig, ax = plt.subplots(4, 1, figsize=(15, 10))
+    ax1,ax2,ax3,ax4 = ax
+
+    # -- plot
+    ax1.plot(ts1)
+
+    ax2.plot(ts2)
+
+    ax3.plot(ts3)
+
+    ax4.plot(ts4)
+
+    fig.canvas.draw()
+
+    plt.show()
+
+    # -- write to file if desired
+    if oname is not None:
+        fig.savefig(oname, format='png',clobber=True)
+
+    return
+
+
+def single_ts(ts_array, clim=None, oname=None):
+    """
+    Plots single lightcurve
     """
 
     # -- set up the plot
     fig = plt.figure(figsize=(10, 10))
 
-    # -- show the image
-    # axim.axis("off")
-    # im = axim.imshow(mask,cmap='gist_gray',clim=clim)
-    # fig.set_title('Light Curve Plot')
-
-    # -- show the lightcurve
-    # axlc.set_xlim(0, curves_obj.curves.shape[0])
+    # -- plot
     plt.plot(ts_array)
 
     fig.canvas.draw()
@@ -105,10 +174,6 @@ def time_series(ts_array, clim=None, oname=None):
         fig.savefig(oname, format='png', clobber=True)
 
     return
-
-
-def bar_graph(data, interval):
-    plt.bar()
 
 
 def compare_curves(curves, ons, offs, night1, night2, v_min=0.3, off=True):
