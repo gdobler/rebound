3c549e21eb6bdc8f0de7b5ecc53f85e7d7e3ffbb
Greg Dobler <greg.dobler@gmail.com>
2017-09-25 22:35:29 -0400

    updated for manhattan

--- rebound/ext/lidar/rasterize.py
+++ rebound/ext/lidar/rasterize.py
@@ -7,7 +7,9 @@ import datetime
 import glob
 import numpy as np
 import pandas as pd
+import geopandas as gp
 import laspy as lp
+from shapely.geometry import Polygon
 from raster_stack import get_origin_minmax
 from utils import get_tile_list, write_header
 
@@ -55,6 +57,31 @@ def mn_rasterize():
                           "MN_raster.bin")
     rast.tofile(oname)
     write_header(oname, params)
+
+
+def get_mn_lidar_tiles():
+    """
+    Get the LiDAR tiles with centers within the Manhattan boundaries.
+    """
+
+    # -- get the lidar tile list and convert to geometries
+    cfile  = os.path.join(os.environ["REBOUND_WRITE"], "lidar_tile_info.csv")
+    coords = pd.read_csv(cfile)
+
+    # -- convert coords to polygons
+    xmin, xmax, ymin, ymax = coords[["xmin", "xmax", "ymin", "ymax"]].values.T
+    polys = [Polygon(((i, k), (i, l), (j, l), (j, k), (i, k))) for 
+             i, j, k, l in zip(xmin, xmax, ymin, ymax)]
+
+    # -- get the borough boundaries
+    bfile = os.path.join(os.environ["REBOUND_DATA"], "nyc_shp", "nybb.shp")
+    boros = gp.read_file(bfile)
+    boros = boros[boros.BoroName == "Manhattan"]
+
+    # -- return tiles that intersect Manhattan
+    tiles = gp.GeoDataFrame({"geometry" : polys})
+
+    return coords[tiles.geometry.intersects(boros.iloc[0].geometry)]
 
 
 def get_lidar_tiles(location="1MT"):
@@ -139,6 +166,63 @@ def rasterize_lidar():
               "rmax" : mm[1][1], "cmin" : mm[0][0], "cmax" : mm[1][0]}
     oname  = os.path.join(os.environ["REBOUND_WRITE"], "rasters", 
                           "BK_raster.bin")
+    rast.tofile(oname)
+    write_header(oname, params)
+
+    return
+
+
+def rasterize_mn_lidar():
+    """
+    Rasterize the MN LiDAR tiles.
+    """
+
+    # -- get the lidar tile names and range
+    coords = get_mn_lidar_tiles()
+    flist  = coords.filename.values
+    nfiles = len(flist)
+    mm     = [[coords.xmin.min(), coords.ymin.min()], 
+              [coords.xmax.max(), coords.ymax.max()]]
+    
+    # -- get the range
+    nrow = int(round(mm[1][1] - mm[0][1] + 0.5)) + 1
+    ncol = int(round(mm[1][0] - mm[0][0] + 0.5)) + 1
+    npix = nrow * ncol
+    
+    # -- initialize the raster and counts
+    rast = np.zeros(npix, dtype=float)
+    cnts = np.zeros(npix, dtype=float)
+    
+    # -- read the tiles
+    for ii, fname in enumerate(flist):
+        print("\rworking on tile {0:3} of {1}".format(ii + 1, nfiles)),
+        sys.stdout.flush()
+        las  = lp.file.File(fname, mode="r")
+        tile = np.vstack((las.x, las.y, las.z)).T
+ 
+        # -- snap to grid
+        rind = (tile[:, 1] - mm[0][1]).round().astype(int)
+        cind = (tile[:, 0] - mm[0][0]).round().astype(int)
+        pind = cind + rind * ncol
+    
+        # -- get the counts in each bin
+        tcnt = np.bincount(pind, minlength=npix)
+
+        # -- update the raster
+        rast[pind] += tile[:, 2]
+        cnts[...]  += tcnt
+
+        # -- close las file
+        las.close()
+    
+    # -- convert sum to mean
+    rast /= cnts + (cnts == 0)
+    
+    # -- write to output
+    params = {"nrow" : nrow, "ncol" : ncol, "rmin" : mm[0][1], 
+              "rmax" : mm[1][1], "cmin" : mm[0][0], "cmax" : mm[1][0]}
+    oname  = os.path.join(os.environ["REBOUND_WRITE"], "rasters", 
+                          "MN_raster.bin")
     rast.tofile(oname)
     write_header(oname, params)
 
