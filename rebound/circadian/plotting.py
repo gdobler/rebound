3dac021ae2bd88d92be6625ff3b6abd01ddc3766
Crull Daynan <dc3445@compute.cusp.nyu.edu>
2017-10-22 23:18:54 -0400

    update plotting

--- rebound/circadian/plotting.py
+++ rebound/circadian/plotting.py
@@ -39,3 +39,53 @@ def simple_plot(img, wav, setlim=[0,1000], asp=0.45):
 	plt.show()
 
 	return
+
+
+def hyper_viz(cube, asp=0.45):
+    """
+    Visualize a hyperspectral scan. Lower plot shows mean light intensity across all wavelengths
+    for full scan. Upper plot shows light intensity by wavelength for pixel hovered over.
+    
+    Args:
+        cube      : data cube of stacked HSI scans (should be nwavs by nrows by ncols)
+        			Cube should be sufficiently cleaned (i.e. data - numpy.median())
+        asp       :
+
+    
+    """
+
+    imgL = cube.mean(0) # mean intensity across all wavelengths
+
+
+    def update_spec(event):
+        if event.inaxes == axim:
+            rind = int(event.ydata)
+            cind = int(event.xdata)
+
+            tspec = cube[:, rind, cind]
+            linsp.set_data(np.arange(0,cube.shape[0]), cube[:, rind, cind])
+            axsp.set_ylim(tspec.min(), tspec.max() * 1.1)
+            axsp.set_title("({0},{1})".format(rind, cind))
+
+            fig.canvas.draw()
+
+
+    # -- set up the plot
+    fig, ax = plt.subplots(2, 1, figsize=(10, 10))
+    axsp, axim = ax
+
+    # -- show the image
+    axim.axis("off")
+    im = axim.imshow(imgL, "gist_gray", interpolation="nearest", aspect=asp)
+    axim.set_title('Stacked HSI scan (mean intensity across wavelength)')
+
+    # -- show the spectrum
+    axsp.set_xlim(0, cube.shape[0])
+    linsp, = axsp.plot(np.arange(0,cube.shape[0]) ,cube[:, 0, 0])
+
+    fig.canvas.draw()
+    fig.canvas.mpl_connect("motion_notify_event", update_spec)
+
+    plt.show()
+
+    return
