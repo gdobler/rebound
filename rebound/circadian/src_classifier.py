9b955b908d0c169c8805e227287a11719f242a73
dcrull <daynan@gmail.com>
2018-02-05 00:24:03 -0500

    add src classifier

--- rebound/circadian/src_classifier.py
+++ rebound/circadian/src_classifier.py
@@ -5,39 +5,52 @@ from __future__ import print_function
 
 import numpy as np
 import os
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
 
+# spatial features
+# spectra (848 channels)
+# Local Moran's I value
 
-def label_pixels(img, n, img_type, src, clip=None):
-    '''
-    Select src vs non-src labels.
+hsi = np.load(os.path.join(os.environ['REBOUND_WRITE'],'circadian','gow_stack_clip_2018.npy'))
 
-    Parameters:
-    ----------
+nosrc_load = np.load(os.path.join(os.environ['REBOUND_WRITE'],'circadian','hsi_nosrc_labels.npy'))
+src_load = np.load(os.path.join(os.environ['REBOUND_WRITE'],'circadian','hsi_src_labels.npy'))
 
-    ipath: 2-d numpy array (img file)
-        A processed image file (cleaned, stacked, etc)
+nosrc = np.empty((nosrc_load.shape[0],nosrc_load.shape[1]+1))
+src = np.empty((src_load.shape[0],src_load.shape[1]+1))
 
-    img_type: str
-        "bb" or "hsi"
+nosrc[:,:-1] = nosrc_load
+src[:,:-1] = src_load
 
-    src: bool
-        True if labeling sources, otherwise False for non-sources.
+nosrc[:,-1] = 0
+src[:,-1] = 1
 
-    Returns:
-        Saves to disk numpy array of labels for <img_type> <src>
-    '''
+y = np.concatenate([src, nosrc], axis=0).round().astype(int)
 
-    plt.imshow(img, clim=[clip[0],clip[1]])
+x = hsi[:,y[:,1],y[:,0]]
 
-    x = plt.ginput(n, show_clicks=True, mouse_pop=3)
+all_data = np.empty((x.shape[1],x.shape[0]+3))
 
-    if src:
-        np.save(os.path.join(os.environ['REBOUND_WRITE'],'circadian','{}_src_labels.npy'.format(img_type)),x)
+all_data[:,:3] = y
 
-    else:
-        np.save(os.path.join(os.environ['REBOUND_WRITE'],'circadian','{}_nosrc_labels.npy'.format(img_type)),x)
+all_data[:,3:] = x.T
 
-    plt.show()
+np.random.shuffle(all_data)
+
+# 75/25 split
+cut = (all_data.shape[0]//4)*3
+
+train =  all_data[:cut,:]
+test = all_data[cut:,:]
+
+# x,y coordinates
+train_loc = train[:,:2]
+test_loc = test[:,:2]
+
+# target variable
+train_y = train[:,2]
+test_y = test[:,2]
+
+# features
+train_x = train[:,2:]
+test_x = test[:,2:]
+
