cdbf5744a71a5404237a067843b0adab086ed167
dcrull <daynan@gmail.com>
2018-02-08 00:03:00 -0500

    updates

--- rebound/circadian/src_classifier.py
+++ rebound/circadian/src_classifier.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 
 import numpy as np
 import os
+from sklearn.svm import SVC
 
 class SourceClassifier(object):
 
@@ -67,7 +68,63 @@ class SourceClassifier(object):
 		self.train_x = self.train[:,3:]
 		self.test_x = self.test[:,3:]
 
-	def scale_data(self, arr):
-		return data / data.max()
+	def scale_data(self, arr, feat_range=[0,1]):
+		'''
+		assumes shape n_samples x n_features
+		default feature range is [0,1] but can be fit to training data min/max
+		'''
+		arr_std = (arr - arr.min(axis=0)) / (arr.max(axis=0) - arr.min(axis=0))
 
+		arr_scaled = arr_std * (feat_range[1] - feat_range[0]) + feat_range[0]
 
+		return arr_std, arr_scaled
+
+	def balanced_f(yhat, ytrue, class1=1.0):
+		prec = ((yhat == class1) & (ytrue == class1)).sum() * 1.0 / (yhat == class1).sum()
+		rec = ((yhat == class1) & (ytrue == class1)).sum() * 1.0 / (ytrue == class1).sum()
+
+		return 2 * ((prec * rec) / (prec + rec))
+
+	def run_svc(self, train_x, train_y, test_x, test_y, penalty=1.0, knl='rbf', deg=3, gm='auto'):
+		sv = SVC(C=penalty, kernel=knl, degree=deg, gamma=gm)
+		model = sv.fit(train_x,train_y)
+		results = sv.predict(test_x)
+
+		accuracy = (results == testy_y).sum()*1.0 / test_y.shape[0]
+
+		f1 = self.balanced_f(results, test_y)
+
+		return results, accuracy, f1
+
+	def kfolds(self, k):
+		merged = np.empty((self.train_x.shape[0],self.train_x.shape[1]+1))
+		merged[:,:-1] = self.train_x
+		merged[:,-1] = self.train_y
+
+		np.random.shuffle(merged)
+
+		cuts = np.linspace(0, merged.shape[0]-1,k).astype(int)
+
+		kdict = {}
+		k_results = {}
+
+		for i in range(k-1):
+			kdict[i] = merged[cuts[i]:cuts[i+1],:]
+
+		for k,v in kdict.items():
+			ktest = v
+			ktrain = np.stack([j for i,j in kdict.items() if i != k], axis=0)
+
+			trx = ktrain[:,:-1]
+			tr_y = ktrain[:,-1]
+
+			tex = ktest[:,:-1]
+			tey = ktest[:,-1]
+
+			trx_scld= self.scale_data(trx)
+			tex_scld = self.scale_data(tex)
+
+			res,acc,f1 = self.run_svc(trx_scld, tr_y, tex_scld, tey, penalty=1.0, knl='rbf', deg=3, gm='auto')
+			k_results[k] = (acc,f1)
+
+		self.k_results = k_results
